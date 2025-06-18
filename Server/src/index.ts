0f32f1ec6e6f16e5f080260faf81c038d94ccfc0
Mateo Zaballo <mzaballo@hotmail.com>
2025-06-17 21:21:31 -0300

    Retoques

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -5,6 +5,7 @@ import authRoutes from './routes/authRoutes';
 import { Usuario } from './models/Usuario';
 import userRoutes from './routes/userRoutes';
 import { Tarea } from './models/Tarea';
+import tareasRoutes from './routes/tareasRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes); // ⬅️ incluye el middleware en rutas como /api/user/perfil
+app.use('/api/tareas', tareasRoutes);
 
 connectToDB().then(() => {
   Usuario.sync(); // crea la tabla si no existe
